6118d001e0397ed185020355a6554d7bcedfd566
byronskout <vyskoutaris@hotmail.com>
2020-02-23 12:43:04 +0000

    Created index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+const sectionsContainer = document.querySelector('.page-sections');
+const sections = document.querySelectorAll('.page-section');
+const nav = document.querySelector('.nav-sections');
+const menu = nav.querySelector('.menu');
+const links = nav.querySelectorAll('.menu-item-link');
+const activeLine = nav.querySelector('.active-line');
+const sectionOffset = nav.offsetHeight + 24;
+const activeClass = 'active';
+let activeIndex = 0;
+let isScrolling = true;
+let userScroll = true;
